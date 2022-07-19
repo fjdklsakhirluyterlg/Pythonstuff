f5eb5f9704c1f844218b9898881944b1cf06950d
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-07-19 19:45:09 +0100

    It actually worked

--- caniscrapegoogle.py
+++ caniscrapegoogle.py
@@ -5,8 +5,9 @@ URL = "https://www.bbc.co.uk/news"
 response  = requests.get(URL)
 
 soup = BeautifulSoup(response.content, "html.parser")
-result = soup.find_all("a", {'class': "gs-c-promo-heading nw-o-link gs-o-bullet__text gs-o-faux-block-link__overlay-link gel-pica-bold gs-u-pl-@xs"}, href=True, )
+result = soup.find_all("a", {'class': "gs-c-promo-heading nw-o-link gs-o-bullet__text gs-o-faux-block-link__overlay-link gel-pica-bold gs-u-pl-@xs"})
 
 for i in result:
-    print(i)
+    x = i.get("href")
+    print(x)
 
