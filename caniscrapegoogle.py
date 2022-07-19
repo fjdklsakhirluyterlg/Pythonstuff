7270b2ed98bc899197e083a3cb4f67015b9f2e85
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-07-19 19:46:03 +0100

    Just prepping it so I can copy to website

--- caniscrapegoogle.py
+++ caniscrapegoogle.py
@@ -2,12 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 
 URL = "https://www.bbc.co.uk/news"
-response  = requests.get(URL)
+response2  = requests.get(URL)
 
-soup = BeautifulSoup(response.content, "html.parser")
-result = soup.find_all("a", {'class': "gs-c-promo-heading nw-o-link gs-o-bullet__text gs-o-faux-block-link__overlay-link gel-pica-bold gs-u-pl-@xs"})
+soup2 = BeautifulSoup(response2.content, "html.parser")
+result2 = soup2.find_all("a", {'class': "gs-c-promo-heading nw-o-link gs-o-bullet__text gs-o-faux-block-link__overlay-link gel-pica-bold gs-u-pl-@xs"})
 
-for i in result:
+for i in result2:
     x = i.get("href")
     print(x)
 
