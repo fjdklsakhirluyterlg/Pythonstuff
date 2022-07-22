a9d323f73db59c596a37ed0f50029069d9bbef06
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-07-22 17:23:34 +0100

    Covid

--- worldometerscrape.py
+++ worldometerscrape.py
@@ -12,13 +12,15 @@ print(result[1].text)
 print(result[2].text)
 
 def get_country_covid(country):
+    x = []
     URL = f"https://www.worldometers.info/coronavirus/country/{country}"
     response = requests.get(URL)
     soup = BeautifulSoup(response.content, "html.parser")
 
     result = soup.find_all("div", "maincounter-number")
-    print(result[0].text)
-    print(result[1].text)
-    print(result[2].text)
+    x.append(result[0].text)
+    x.append(result[1].text)
+    x.append(result[2].text)
+    return x
 
 get_country_covid("uk")
