3e6118c7df2098644e6f8d073028971fe60a92ce
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-07-22 17:04:44 +0100

    Worldometers scraping

---
+++ worldometerscrape.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://www.worldometers.info/coronavirus/"
+
+response = requests.get(URL)
+soup = BeautifulSoup(response.content, "html.parser")
+
+result = soup.find_all("div", "maincounter-number")
+print(result[0].text)
