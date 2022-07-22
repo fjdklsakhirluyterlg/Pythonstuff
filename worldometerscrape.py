78a8e46ad62af831d4fee6c1b70d51c4f3637fe0
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-07-22 17:22:11 +0100

    Coivd is covid

--- worldometerscrape.py
+++ worldometerscrape.py
@@ -11,3 +11,14 @@ print(result[0].text)
 print(result[1].text)
 print(result[2].text)
 
+def get_country_covid(country):
+    URL = f"https://www.worldometers.info/coronavirus/country/{country}"
+    response = requests.get(URL)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    result = soup.find_all("div", "maincounter-number")
+    print(result[0].text)
+    print(result[1].text)
+    print(result[2].text)
+
+get_country_covid("uk")
